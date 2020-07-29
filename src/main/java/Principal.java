8e93680d74d7a45fc44ac66872f12a03975ec3a7
zurum ogbonda <zurum@zurums-MacBook-Pro.local>
2020-07-29 17:13:05 +0100

    added comments to principal class

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -4,17 +4,20 @@ public class Principal extends PersonBio{
         super(name, age, id);
     }
 
+//    A method to signify that a Principal can create a class
     public void createClass(String scClass){
         SchoolClasses schoolClasses = new SchoolClasses();
         System.out.println(schoolClasses.createClasses(scClass));
     }
 
+//    A method for admitting an Applicant based on Age and Test score
     public String AdmitApplicant(){
         Applicants applicants = new Applicants("John",19,"007",67);
         System.out.println(applicants.knowYourAdmissionStatus());
         return applicants.knowYourAdmissionStatus();
     }
 
+//    A method for Expelling a Student based on Character
     public String expelStudent(String name){
         DevStudent devStudent = new DevStudent("John",23,"008",true);
         if(devStudent.isCharacter()){
--- src/main/java/Staffs.java
+++ src/main/java/Staffs.java
@@ -1,7 +1,5 @@
 public class Staffs extends PersonBio {
-
     public Staffs(String name, int age, String id) {
         super(name, age, id);
     }
-
 }
